a0dd831e1de3db431802d6de1bac25dca93a453d
Dusk <markafullton@outlook.com>
2020-09-07 14:41:31 -0700

    Added search book function

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Searchbar from "./Searchbar";
 
 class Header extends React.Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Searchbar />
       </div>
     );
   }
--- client/src/containers/Search/index.js
+++ client/src/containers/Search/index.js
@@ -1,10 +1,72 @@
 import React from "react";
+import API from "../../utils/API";
+import SearchResult from "../../components/SearchResult";
+import Searchbar from "../../components/Searchbar";
 
 class Search extends React.Component {
+  state = {
+    search: "",
+    books: [],
+    error: "",
+    message: "",
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ search: e.target.value });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    API.getGoogleSearchBooks(this.state.search)
+      .then((res) => {
+        if (res.data.items === "error") {
+          throw new Error(res.data.item);
+        } else {
+          let results = res.data.items;
+
+          results = results.map((result) => {
+            result = {
+              key: result.id,
+              id: result.id,
+              title: result.volumeInfo.title,
+              author: result.volumeInfo.authors,
+              description: result.volumeInfo.description,
+              image: result.volumeInfo.imageLinks.thumbnail,
+              link: result.volumeInfo.infoLink,
+            };
+
+            return result;
+          });
+          this.setState({ books: results, error: "" });
+        }
+      })
+      .catch((err) => this.setState({ error: err.items }));
+  };
+
+  handleSavedButton = (e) => {
+    e.preventDefault();
+    console.log(this.state.books);
+    let savedBooks = this.state.books.filter((book) => book.id === e.target.id);
+
+    savedBooks = savedBooks[0];
+    API.saveBooks(savedBooks)
+      .then(this.setState({ message: alert("Your book is saved") }))
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <div>
-        <p>Search</p>
+        <Searchbar
+          search={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+        />
+        <SearchResult
+          books={this.state.books}
+          handleSavedButton={() => this.handleSavedButton}
+        />
       </div>
     );
   }
