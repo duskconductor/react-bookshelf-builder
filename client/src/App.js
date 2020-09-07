9fdcdecd6ffb55b059abc256c5d700314b85bf64
Dusk <markafullton@outlook.com>
2020-09-07 11:40:51 -0700

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Bookshelf from "./containers/bookshelf";
+import Login from "./containers/Login";
+import NotFound from "./containers/NotFound";
+import Register from "./containers/Register";
+import Search from "./containers/Search";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <div>
+            <p>Header and etc</p>
+          </div>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Search} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/bookshelf" component={Bookshelf} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+          <div>
+            <p>Footer and etc</p>
+          </div>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/containers/bookshelf/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Bookshelf extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Bookshelf</p>
+      </div>
+    );
+  }
+}
+
+export default Bookshelf;
