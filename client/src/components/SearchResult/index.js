6ef8e0594e731c6f4b3a7db08224eeedcdda5944
Mark <the.mark.fullton@gmail.com>
2020-09-07 22:31:24 -0700

    Added divider scss

--- client/src/components/SearchResult/index.js
+++ client/src/components/SearchResult/index.js
@@ -4,27 +4,28 @@ const SearchResult = (props) => {
   return props.books.length === 0 ? (
     <div className="searchResultWrapper">
       <div className="searchResultContent">
-        <p></p>
+        <p>Use the search bar above to search for any book.</p>
       </div>
     </div>
   ) : (
     <div className="searchResultWrapper">
       <div className="searchResultContent">
+        <h2>
+          <i class="fas fa-search"></i> - {props.searched}
+        </h2>
         {props.books.map((book) => {
           return (
             <div className="bookContainer" id={book.title}>
+              <h3>{book.title}</h3>
+              <h5>{book.author}</h5>
               <div className="bookFirstColumn">
-                <div className="bookTitle">{book.title}</div>
                 <div className="bookImageContainer">
                   <img src={book.image} alt={book.title} />
                 </div>
               </div>
               <div className="bookSecondColumn">
-                <h3>{book.title}</h3>
-                <h5>{book.author}</h5>
-                <p>{book.description}</p>
-                <br />
                 <p>
+                  {book.description.substring(0, 300)}...{" "}
                   <a href={book.link} target="_blank">
                     Learn More
                   </a>
